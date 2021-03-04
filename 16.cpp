65b48bae889c506b134fc92252a5eabe5a83b139
Yukta Tiwari <61117440+Yuktaaa@users.noreply.github.com>
2021-03-04 18:19:16 +0530

    Add files via upload

---
+++ 16.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, i, j, sum = 0;
+    cout << "Enter the numbers: ";
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = i - 1; j >= 1; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (i = n - 1; i >= 1; i--)
+    {
+        for (j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (j = i - 1; j >= 1; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
